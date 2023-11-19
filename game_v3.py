114697a60552a1c59a00c77f61e7e76e7d5e9d56
seldaria11 <seldaria11@gmail.com>
2023-11-19 23:01:07 +0300

    Initial commit

---
+++ game_v3.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+def game_core_v3(number: int = 1) -> int:
+    """
+    Args:
+        number (int, optional): Загаданное число. Defaults to 1.
+
+    Returns:
+        int: Число попыток
+    """
+    # Ваш код начинается здесь
+    start = 0
+    end = 101
+    count = 1
+    while True:
+        predict = (start + end) // 2
+        if number == predict:
+            break
+        count += 1
+        if number > predict:
+            start = predict
+        elif number < predict:
+            end = predict
+    # Ваш код заканчивается здесь
+    return count
+
+def score_game(game_core_v3) -> int:
+    """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
+
+    Args:
+        random_predict ([type]): функция угадывания
+
+    Returns:
+        int: среднее количество попыток
+    """
+    count_ls = []
+    #np.random.seed(1)  # фиксируем сид для воспроизводимости
+    random_array = np.random.randint(1, 101, size=(1000))  # загадали список чисел
+
+    for number in random_array:
+        count_ls.append(game_core_v3(number))
+
+    score = int(np.mean(count_ls))
+    print(f"Ваш алгоритм угадывает число в среднем за:{score} попыток")
+    return score
+
+if __name__ == "__main__":
+    score_game(game_core_v3)
+
+
